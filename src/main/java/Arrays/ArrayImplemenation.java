260d10a9595a3f48ccab1f239b067f6b7b6864cc
Ashika Chakravorty <ashika.chak@outlook.com>
2023-08-11 12:20:29 +0530

    two easy questions of Array

---
+++ src/main/java/Arrays/ArrayImplemenation.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayImplemenation {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        int[] arr2 = { 2, 5, 6, 9, 7, 4, 3 };
+
+    }
+}
