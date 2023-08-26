889e2e94437e57ce6b6ef14d4c9326316a36549e
Ashika Chakravorty <ashika.chak@outlook.com>
2023-08-26 18:18:39 +0200

    medium array problem - find duplicates (only method desc)

---
+++ src/main/java/Arrays/FindDuplicates.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class FindDuplicates {
+
+    //GFG
+    // Given an array a of size N which contains
+    // elements from 0 to N-1, you need to find
+    // all the elements occurring more than once
+    // in the given array. Return the answer in
+    // ascending order. If no such element is
+    // found, return list containing [-1].
+
+    public static ArrayList<Integer> duplicates(int[] arr, int n) {
+
+        //without using extra space
+        ArrayList<Integer> list = new ArrayList<>();
+
+        return list;
+
+        // brute force with extra array - add count of occourances
+        // to extra array
+    }
+}
