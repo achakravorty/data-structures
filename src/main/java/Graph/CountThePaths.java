7f587c621cf24673caaf0fb5186decb9ca99ad3e
Ashika Chakravorty <ashika.chak@outlook.com>
2023-10-31 12:38:13 +0100

    Graph problem - count possible paths from given edges in a graph (INCOMPLETE)

---
+++ src/main/java/Graph/CountThePaths.java
@@ -0,0 +1,24 @@
+package Graph;
+
+public class CountThePaths {
+
+    //GFG//
+    // Given a directed acyclic graph(DAG) with n nodes labeled
+    // from 0 to n-1. Given edges, s and d ,count the number of
+    // ways to reach from s to d.There is a directed Edge from
+    // vertex edges[i][0] to the vertex edges[i][1].
+
+    /**
+     * Function to find count of paths of a DAG to reach from given
+     * 2 edges.
+     * @param edges list of edges of graph
+     * @param n number of edges of graph
+     * @param s start edge of path
+     * @param d end edge of path
+     * @return the number of ways to reach from s to d.
+     */
+    public int possible_paths(int[][] edges, int n, int s, int d) {
+        //TODO
+        return 0;
+    }
+}
